0943866d4acff4335ed895c09858fe8a37278e75
Victor <victor.meyo@student.moringaschool.com>
2025-04-22 12:39:34 +0300

    Final submission

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    sequence = [0,1]
+
+    while len(sequence) < length:
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+
+    print(sequence[:length])       
+    
