169ebd9025e2775495924679eb42dd36c2cd78fb
Victor <victor.meyo@student.moringaschool.com>
2025-06-30 12:13:33 +0300

    Additional comments added

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+# Function that prints a list of Fibonacci sequences. 
 
-def print_fibonacci(length):
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21]
+# Logic: [[0]=0, [1]=1+0, [2]=1+0, [3]=1+1, [4] =1+2, [5]=2+3......]
+
+def print_fibonacci(length): # Takes an argument length that defines how many fibonacci sequences you want to print
     if length <= 0:
-        print([])
+        print([]) # No element
         return
 
-    sequence = [0,1]
+    sequence = [0,1] # Initialize the Fibonacci series with the 1st 2 no.s
 
     while len(sequence) < length:
         next_value = sequence[-1] + sequence[-2]
